acce36b61a98d7b41362960a4c822567f3de8973
sushantku <sushant.kashyap10@gmail.com>
2023-09-08 17:56:12 +0530

    Update app.js
    git add app.js
    git commit -m "change message"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my 1st project'));
+app.get('/', (req, res) => res.send('This is my 1st project and 2nd project'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
